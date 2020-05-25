85346333e512e633bfaa838b15ceb41a8b45b237
smokyFloky <56809153+smokyFloky@users.noreply.github.com>
2020-05-25 14:11:24 -0400

    git commit

--- src/MaibClass.java
+++ src/MaibClass.java
@@ -3,15 +3,17 @@ import java.util.Scanner;
 
 public class MaibClass {
 
-    public static int doubleInterval;
+    public  static int doubleInterval;
     public static void main(String[] args) {
 
 
-        System.out.println("Veuillez appuyer sur : \n 1 pour jouer \n 2 pour voir les stats \n 3 pour sortir \n 4 rejouer");
+        System.out.println("Veuillez appuyer sur : \n 1 pour jouer  \n 3 pour sortir");
         Scanner scanMenu = new Scanner(System.in);
         int f = scanMenu.nextInt();
         int index = 0;
         int globScore = 0;
+        Random r = new Random();
+        doubleInterval = r.nextInt(5); //generation num aleatoirement
         switch (f) {
 
             case 1:
@@ -27,18 +29,31 @@ public class MaibClass {
                     }}
 
 
-                System.out.println("Veuillez appuyer sur : \n 2 pour voir les stats \n 4 pour sortir");
+                System.out.println("Veuillez appuyer sur : \n 2 pour voir les stats \n 4 pour rejouer \n 5 pour sortir");
                 Scanner scan = new Scanner(System.in);
                 int y = scan.nextInt();
 
              switch (y) {
+
                  case 2:
                      System.out.println("Score Board");
+
                      System.out.println("Votre score est " + globScore);
                      break;
+                 case 4:
+                     doubleInterval*=2;
+                     System.out.println("jouer");
+                        globScore=0;
+                     while(globScore<100) {
+                         //glob = scoreBoard();
+                         if(jouer()){
+                             globScore+=100;
+                             System.out.println("Votre score est " +   globScore);
+                         }}
 
-                 case 5:
                      break;
+                 case 5:
+                     System.out.println("Veuillez appuyer sur : \n 5 pour sortir");
 
              }
 
@@ -51,19 +66,7 @@ public class MaibClass {
                 System.out.println("exit");
 
                 break;
-            case 4:
-                doubleInterval*=2;
-                System.out.println("jouer");
 
-
-
-                while(globScore<100) {
-                    //glob = scoreBoard();
-                    if(jouer()){
-                        globScore+=100;
-                        System.out.println("Votre score est " +   globScore);
-                    }}
-                break;
 
 
         }
@@ -81,11 +84,10 @@ public class MaibClass {
             System.out.println("jaffiche pour tester " + type);
 
             int f = Integer.parseInt(type);
-            Random r = new Random();
-           doubleInterval = r.nextInt(5);
 
 
-            System.out.println(doubleInterval);
+
+            //System.out.println(doubleInterval); /** Affichage de num aléatoire **/
 
             if (f < doubleInterval) {
                 System.out.println("numero choisi est plus petit que le numero desiré");
